d361d8e2f4e017b02cd752a590c4efc08b3cbe56
Michael Mwai <michaeldmwai@gmail.com>
2024-11-26 17:50:19 +0300

    Add base App component with routing placeholder

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import Home from "./pages/Home";
+import Favorite from "./pages/Favorites";
+import Navbar from "./components/Navbar";
+import "./css/App.css";
+
+function App() {
+  return (
+    <MovieProvider>
+      <div>
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorite />} />
+          </Routes>
+        </main>
+      </div>
+    </MovieProvider>
+  );
+}
+
+export default App;
